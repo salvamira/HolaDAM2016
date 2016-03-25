136eb592b3ebab5f1d3a1f96c7a217495f5cba51
Salva Mira <smira@iesperemaria.com>
2016-03-25 13:24:34 +0100

    Res de res

--- app/src/main/java/net/somtic/holadam/MainActivity.java
+++ app/src/main/java/net/somtic/holadam/MainActivity.java
@@ -2,12 +2,16 @@ package net.somtic.holadam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        TextView nuevoTexto = new TextView(this);
+        nuevoTexto.setText("Hola DAM desde Java");
+        setContentView(nuevoTexto);
     }
 }
